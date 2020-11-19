6a799f4355c2c6379b535de16e54131bef018818
abhin <abhinandanchivate2000@gmail.com>
2020-11-19 10:07:02 +0530

    commit

---
+++ inventory-discovery-service/src/main/java/com/dhl/inventorydiscoveryservice/InventoryDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dhl.inventorydiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryDiscoveryServiceApplication.class, args);
+	}
+
+}
